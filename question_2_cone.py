a15e61b5ecad3ad9f5419e7ac560736013da2e19
Ransley-Wong-Lieng-Jun <163949368+Ransley-Wong-Lieng-Jun@users.noreply.github.com>
2024-10-15 08:54:59 +0800

    Update question_2_cone.py 2

--- question_2_cone.py
+++ question_2_cone.py
@@ -6,13 +6,13 @@
 from math import pi
 
 def volume_cone(radius, height):
-    volume = (1/ 3) * pi * radius ** 2 * height
+    volume = (1/3) * pi * radius ** 2 * height
     return volume
 
 def main():
     a = float(input("Input radius: ")) 
     b = float(input("Input height: ")) 
-    volume = volume_cone(a,b) 
+    volume = volume_cone(a, b) 
     print(f"Volume of cone = { volume :.2f}") 
 
 # Don't change the code below!
